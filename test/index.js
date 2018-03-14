222c9c64e6a0adfd7f43eeac390f8200b6db0605
James <james@linnegar.com>
2018-03-14 22:36:41 +0000

    Port for test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 import http from 'http';
 import assert from 'assert';
+const PORT = process.env.PORT || 5000
 
 import '../lib/index.js';
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+    http.get('http://127.0.0.1:'+PORT, res => {
       assert.equal(200, res.statusCode);
       done();
     });
