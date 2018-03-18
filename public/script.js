bac596a51726e3eb5c7d166db84fa6a10547df57
James <james@linnegar.com>
2018-03-18 21:11:55 +0000

    Websockets spike

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const ws = new WebSocket('ws://localhost:5000');
+const text = document.getElementById("text");
+ws.onopen = function open() {
+  console.log('connected');
+  ws.send(Date.now());
+};
+
+ws.onclose = function close() {
+  text.innerText = 'disconnected';
+};
+
+ws.onmessage = function incoming(event) {
+  text.innerText = event.data;
+};
